4179a49cee466214939c6b4d4ee243259b4436a5
jondeaton <jonpauldeaton@gmail.com>
2017-01-26 10:47:27 -0800

    Development

--- src/prediction_handler.py
+++ src/prediction_handler.py
@@ -1,38 +1,50 @@
+#!/usr/bin/env python
 
+import time
+import pandas as pd
+import logging
 
+logging.basicConfig(format='[%(levelname)s][%(funcName)s] - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
+__author__ = "Jon Deaton"
+__email__ = "jdeaton@stanford.edu"
 
+class PredictionHandler(object)
 
+	def __init__(self):
 
+		self.risk_series = pd.Series()
 
+		logger.info("Initializing user...")
+		self.user = user.MonitorUser(load=True)
+		logger.info("Monitor User: {name}".foramt(name=self.user.name))
 
+		logger.info("Initializing monitor...")
+		self.monitor = monitor.HeatStrokeMonitor()
+		logger.info("Monitor initialized")
 
+		logger.info("Initializing predictor...")
+		self.predictor = predictor.HeatStrokePredictor()
+		logger.info("Predictor initialized")
 
-
-# TODO
-
-
-
-
-
-
-
-
-
+	def start_data_collection(self):
+		monitor.read_data_from_port()
 
 
 
 
 def main():
+	import argparses
 	script_description = "This script reads data from a monitor and uses"
     parser = argparse.ArgumentParser(description=script_description)
 
     input_group = parser.add_argument_group("Inputs")
-    input_group.add_argument('-in', '--input', help='Input spreadsheet with case data')
-    input_group.add_argument('-sheet', '--sheetname', default='Individualized Data', help="Name of sheet in excel file")
+    input_group.add_argument('-in', '--input', required=False, help='Input spreadsheet with case data')
 
     output_group = parser.add_argument_group("Outputs")
-    output_group.add_argument("-out", "--output", help="Output")
+    output_group.add_argument("-out", "--output", help="Predictoin Output")
 
     options_group = parser.add_argument_group("Opitons")
     options_group.add_argument("-f", "--fake", action="store_true", help="Use fake data")
