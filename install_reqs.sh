0a4688e5dd8fa814a38aaead2afe2c000711966c
jondeaton <jonpauldeaton@gmail.com>
2017-02-27 20:46:01 -0800

    Added installation script

---
+++ install_reqs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script is for installing the pip packages necessary
+# to run the python script in this repository. This script
+# will not install Anaconda 3 for you.
+
+pip install update
+pip install pyserial
+pip install meteocalc
+pip install coloredlogs
+pip install emoji
+pip install termcolor
